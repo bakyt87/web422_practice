51436be8f3462fd7596eacdfeece8e85741fae6b
bakyt kurmanov <bakytkurmanov87@gmail.com>
2020-08-03 23:13:55 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DriverComponent } from './driver/driver.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'driver', component: DriverComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
